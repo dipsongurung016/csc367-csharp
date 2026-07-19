3674f7a6b6b53e281804e34e65b398d545bb99a9
dipsongurung016 <dipsong131@gmail.com>
2026-07-19 23:29:02 +0545

    update

--- 02_constants/Program.cs
+++ 02_constants/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        const double pi=3.14159;
+        Console.WriteLine(pi);
+    }
+}
