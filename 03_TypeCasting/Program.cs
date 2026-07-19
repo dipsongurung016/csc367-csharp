7374324eb08be4636141a13a7bf96c9f6346b525
dipsongurung016 <mail@dipsongurung.com.np>
2026-07-19 23:49:45 +0545

    Merge remote repository

--- 03_TypeCasting/Program.cs
+++ 03_TypeCasting/Program.cs
@@ -1,4 +1,10 @@
-﻿double a = 3.14;
-int b = Convert.ToInt32(a);
-//int c = 10;
-Console.WriteLine(b);
+﻿class Program
+{
+    static void Main()
+    {
+        double a = 3.14;
+        int b = Convert.ToInt32(a);
+        //int c = 10;
+        Console.WriteLine(b);
+    }
+}
