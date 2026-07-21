fcb920ba9db5ad517d30553dadd71ae638929cc5
dipsongurung016 <mail@dipsongurung.com.np>
2026-07-21 07:27:02 +0545

    new repo created

--- 01_Variables/obj/Debug/net10.0/01_Variables.AssemblyInfo.cs
+++ 01_Variables/obj/Debug/net10.0/01_Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fb6750c1e6dca27b344794d98aef7e1d4c9661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4279bd6ab6a073ae648f1dcdee9a4cb538b769")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 06_exception handling/Program.cs
@@ -0,0 +1,46 @@
+﻿// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         try
+//         {
+//             int a = 10;
+//             int b = 0;
+//             int result = a / b;
+//             Console.WriteLine(result);
+//         }
+//         catch (DivideByZeroException ex)
+//         {
+//             //Console.WriteLine("Error: " + ex.Message);
+//             Console.WriteLine("Error: " + ex.Message);
+//         }
+//         // finally
+//         // {
+//         //     Console.WriteLine("Program completed.");
+//         // }
+//     }
+// }
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int[] age = new int[3];
+            int myage = age[4];
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("exception occured : " + ex.Message);
+        }
+        // finally
+        // {
+        //     Console.WriteLine("Program completed.");
+        // }
+    }
+}
